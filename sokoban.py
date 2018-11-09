aadb5f1062276e6002d6630bbd15fd3caa242114
navarka.all@gmail.com <navarka.all@gmail.com>
2018-11-09 20:11:19 +0300

    Sokoban update

--- sokoban.py
+++ sokoban.py
@@ -8,19 +8,51 @@ class Sokoban:
         Sokoban class main game
     """
     def __init__(self):
-        pass
+        self.BOX = "💩 "
+        self.HER = "😐 "
+        self.OBS = "██"
+        self.EMP = "  "
+        self.DES = "🌐 "
 
     def loop(self):
         while(True):
+            self.draw_level()
             event = self._mainWin.getkey()
-            self._mainWin.addstr(event)
-            self._mainWin.addstr('\n')
             if event == 'q':
                 break
-            elif event == '^C':
-                break
             elif event == 'KEY_UP':
-                lev
+                self._level.move_up()
+            elif event == 'KEY_DOWN':
+                self._level.move_down()
+            elif event == 'KEY_LEFT':
+                self._level.move_left()
+            elif event == 'KEY_RIGHT':
+                self._level.move_right()
+
+
+    def draw_level(self):
+        c_y = 0
+        c_x = 0
+        for line in self._level:
+            c_y += 1
+            c_x = 0
+            # self._gameWin.move(c_line, 0)
+            for symbol in line:
+                c_x += 2
+                c_sumbol = "  "
+                if symbol == 0:
+                    c_sumbol = self.EMP
+                elif symbol == 1:
+                    c_sumbol = self.HER
+                elif symbol == 2:
+                    c_sumbol = self.BOX
+                elif symbol == 3:
+                    c_sumbol = self.OBS
+                elif symbol == 3:
+                    c_sumbol = self.DES
+                self._gameWin.addstr(c_y, c_x, c_sumbol)
+
+
 
     def run(self):
         self._mainWin = curses.initscr()
@@ -29,19 +61,19 @@ class Sokoban:
         # curses.cbreak() # WTF
         self._mainWin.keypad(True)
         gameWinY, gameWinX= self._mainWin.getmaxyx()
-        gameWinY = (gameWinY-25) // 2
-        gameWinX = (gameWinX-50) // 2
-        self._mainWin.addstr(str(gameWinX))
-        self._mainWin.addstr(str(gameWinY))
-        self._gameWin = self._mainWin.subwin(25,50, gameWinY, gameWinX)
+
+        # self._levelManager = LevelManager()
+        # for level in self._levelManager:
+        self._level = Level() ## change for loop later
+        gameWinY = (gameWinY) // 2 - self._level.height
+        gameWinX = (gameWinX) // 2 - self._level.width
+        self._gameWin = self._mainWin.subwin(self._level.height+4, self._level.width*2+4, gameWinY, gameWinX)
         self._gameWin.border()
-        self._gameWin.addstr(1, 1, '░░')
-        self._gameWin.addstr(2, 2, '░░')
 
         try:
-            self.loop()
-        except Exception:
-            pass
+            result = self.loop()
+        except Exception as e:
+            print(e)
 
         self._mainWin.keypad(False)
         # curses.nocbreak() # WTF
